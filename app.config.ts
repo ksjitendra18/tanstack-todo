a5c2a556903305422f366fc9bf76f112cbd3a640
Jitendra <jitendraks1803@gmail.com>
2025-04-15 11:23:50 +0530

    added cf pages

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "unenv";
 
 export default defineConfig({
   tsr: {
     appDirectory: "src",
+  },
+  server: {
+    preset: "cloudflare-pages",
+    unenv: cloudflare,
   },
   vite: {
     plugins: [
